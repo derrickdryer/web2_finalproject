99b36f580c2dfb9f5c287dca7e3f8f40f684b0ca
Derrick Dryer <derrickdryer@proton.me>
2024-04-03 22:32:45 -0400

    Added a small bit of javascript for the database connection notification

--- php/db_connect.php
+++ php/db_connect.php
@@ -7,10 +7,27 @@
     # Try and catch any errors
     try {
         $db = new PDO($dsn, $username, $password);
-        echo '<center><p> You are connected. </p></center>';
+        echo '<div id="success-notification" style="display: none;">You are connected.</div>';
+        echo '<script type="text/javascript">
+                var notification = document.getElementById("success-notification");
+                notification.style.display = "block";
+                notification.style.backgroundColor = "green";
+                setTimeout(function() {
+                    notification.style.display = "none";
+                }, 2500);
+            </script>';
     }
     catch (PDOException $e){
         $error_message = $e->getMessage();
-        echo '<center><p> Connection failed due to error : $error_message </p></center>';
+        echo '<link rel="stylesheet" type="text/css" href="./css/main.css">';
+        echo '<div id="error-notification" style="display: none;">Connection failed due to error: ' . $error_message . '</div>';
+        echo '<script type="text/javascript">
+                var notification = document.getElementById("error-notification");
+                notification.style.display = "block";
+                notification.style.backgroundColor = "red";
+                setTimeout(function() {
+                    notification.style.display = "none";
+                }, 2500);
+            </script>';
     }
 ?>
